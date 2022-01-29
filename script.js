72513cc2d19ba064974ab954038c47e3303aeee6
andressavillela <villelaandressa00@gmail.com>
2022-01-29 16:13:42 -0300

    Nova atualização
    Limpeza e simplificação de código.

--- script.js
+++ script.js
@@ -26,4 +26,4 @@ function calculateTip(event) {
 document.getElementById('totalTip').style.display = "none";
 document.getElementById('each').style.display = "none";
 
-document.getElementById('tipsForm').addEventListener('submit', calculateTip);
+document.querySelector('form').addEventListener('submit', calculateTip);
